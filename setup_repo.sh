d32cb8bdb0f63f92c4585a4cd594d0dbf5a00876
DavidSpek <vanderspek.david@gmail.com>
2021-04-03 16:42:02 +0200

    Add metallb

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-yq e -i ".spec.source.repoURL = \"$1\"" kubeflow.yaml
-for filename in ./argocd-applications/*.yaml; do
-    yq e -i ".spec.source.repoURL = \"$1\"" $filename
-done
+
+# set secretkey for metallb
+yq eval -i ".stringData.secretkey = \"$(openssl rand -base64 128)\"" metallb/secret.yaml
+
+if [ -z "$1" ]
+    then
+        echo "no repo URL provided, using upstream"
+    else
+        yq e -i ".spec.source.repoURL = \"$1\"" kubeflow.yaml
+        for filename in ./argocd-applications/*.yaml; do
+            yq e -i ".spec.source.repoURL = \"$1\"" $filename
+        done
+fi
